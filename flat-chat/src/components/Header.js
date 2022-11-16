465f574ca0d602dcfc2dbbfaf2806928c2ba5e3b
mason white <masondwhite1@gmail.com>
2022-11-16 11:14:27 -0700

    New Heder Layout

--- flat-chat/src/components/Header.js
+++ flat-chat/src/components/Header.js
@@ -4,10 +4,10 @@ import {NavLink} from "react-router-dom";
 function Header() {
     const linkStyles = {
         textAlign: "center",
-        display: "inline-block",
+        // display: "inline-block",
         width: "75px",
         padding: "12px",
-        margin: "0 100px 6px",
+        margin: "0 80px 6px",
         background: "transparent",
         textDecoration: "none",
         color: "white",
@@ -15,14 +15,14 @@ function Header() {
 
   return (
     <header id= "header">
-    <NavLink className= "about" to= "/about" style={linkStyles}>About </NavLink>
-    <NavLink to= "/" style={linkStyles}>Home</NavLink>
-    <span className="logo">{"//"}</span>
-    <NavLink to= "/chat" style={linkStyles}>Chat</NavLink>
-    <NavLink to= "/contact" style={linkStyles}>Contact</NavLink>
-    <h1 className= "title">
-    The Flat Chat
-    </h1>
+      {/* <span className="logo">{"//"}</span> */}
+      <span className="logo">{"//The Flat Chat"}</span>
+      <NavLink to= "/" style={linkStyles}>|Home|</NavLink>
+      <NavLink to= "/chat" style={linkStyles}>|Chat|</NavLink>
+      {/* <span className="logo">{"//"}</span> */}
+      <NavLink className= "about" to= "/about" style={linkStyles}>|About|</NavLink>
+      <NavLink to= "/contact" style={linkStyles}>|Contact|</NavLink>
+      {/* <h1 className= "title">The Flat Chat</h1> */}
     </header>
   );
 }
