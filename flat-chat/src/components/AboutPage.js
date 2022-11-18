c60d1bf3cbee04803e2e0077a4fb1cd068285f01
Charles <charlie.david@me.com>
2022-11-18 12:07:26 -0700

    7th Commit

--- flat-chat/src/components/AboutPage.js
+++ flat-chat/src/components/AboutPage.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function AboutPage() {
-  return <h1 className= "about-text">About Us </h1>;
+  return <div>
+  <h1 className= "about-text">About Us </h1>
+  <h2>The FlatChat is your go to webpage for all Flatiron School communications</h2>
+  <h3>If you have any questions on a specific topic, please fill out the form on the "Chat" page and wait for a fellow school-mate to come for the rescue!</h3>
+</div>;
 }
 
 export default AboutPage;
--- flat-chat/src/components/ContactPage.js
+++ flat-chat/src/components/ContactPage.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function ContactPage() {
   return(
-  <h1 className="contact">Contact us</h1>
+    <div>
+    <h1 className= "contact-text">Contact Us </h1>
+    <h2>Phone Number: 303-555-6565</h2>
+    <h2>Email: flatironschooltopgang@gmail.com</h2>
+    <h2>Address: 225 Blake St, Denver CO 80122</h2>
+    </div>
 )}
 
 export default ContactPage;
